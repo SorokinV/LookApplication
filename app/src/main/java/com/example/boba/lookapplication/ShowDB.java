37607af0938783bd46a05cb9f55c6fa89afe795d
boba2 <sorokinv@hotmail.com>
2015-07-06 10:26:47 +1000

    SQL change 4

--- app/src/main/java/com/example/boba/lookapplication/ShowDB.java
+++ app/src/main/java/com/example/boba/lookapplication/ShowDB.java
@@ -1,33 +1,54 @@
 package com.example.boba.lookapplication;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class ShowDB extends ActionBarActivity {
+
+    boolean OKEraseDB = false;
+
+    DB1 database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_db);
 
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        OKEraseDB  = sharedPref.getBoolean("pref_erase_db", OKEraseDB);
+
+        database = new DB1(this);
         DrawScreen();
 
     }
 
     void DrawScreen () {
 
-        DB1 database = new DB1(this);
         TextView countRecords = (TextView) findViewById(R.id.countRecords);
         TextView countBSSID   = (TextView) findViewById(R.id.count_BSSID_records);
+        Button   eraseDB      = (Button)   findViewById(R.id.dbErase);
 
-        countRecords.setText(""+database.countAllRecords());
-        countBSSID.setText(""+database.countAllBSSID());
+        countRecords.setText("" + database.countAllRecords());
+        countBSSID.setText("" + database.countAllBSSID());
+        eraseDB.setClickable(OKEraseDB);
+        eraseDB.setEnabled(OKEraseDB);
 
     }
+
+    public void ClickEraseDB (View view) {
+        database.clearDataBase();
+        DrawScreen();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
